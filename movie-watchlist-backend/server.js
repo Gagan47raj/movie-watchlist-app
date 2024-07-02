0e6966184171d8c3395ec3101f946beec82c7106
Gagan <100960759+Gagan47raj@users.noreply.github.com>
2024-07-02 08:29:21 +0530

    Update server.js

--- movie-watchlist-backend/server.js
+++ movie-watchlist-backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const moviesRoutes = require('./routes/movies');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors(
     {
