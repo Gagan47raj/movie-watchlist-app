7a7f5dff121587b8534c1fac71900099f32ba946
Gagan <100960759+Gagan47raj@users.noreply.github.com>
2024-07-02 10:56:30 +0530

    Update movieAction.js

--- movie-watchlist-frontend/src/state/action/movieAction.js
+++ movie-watchlist-frontend/src/state/action/movieAction.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+
 export const getMovies = () => async (dispatch) => {
   const response = await axios.get('http://localhost:5000/movies');
   dispatch({ type: 'GET_MOVIES', payload: response.data });
