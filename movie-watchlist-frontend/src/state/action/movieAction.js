eae0bbb126eb4bb96cca5867c59b8d723f0782c7
Gagan <100960759+Gagan47raj@users.noreply.github.com>
2024-07-02 11:04:42 +0530

    Update movieAction.js

--- movie-watchlist-frontend/src/state/action/movieAction.js
+++ movie-watchlist-frontend/src/state/action/movieAction.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = 'https://movie-watchlist-app-server.vercel.app' || 'http://localhost:5000';
 
 export const getMovies = () => async (dispatch) => {
   const response = await axios.get('http://localhost:5000/movies');
