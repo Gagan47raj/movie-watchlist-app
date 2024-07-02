9970260f6a90fa1e132065ac41f8b9c6d6272099
Gagan <100960759+Gagan47raj@users.noreply.github.com>
2024-07-02 11:58:46 +0530

    Update movies.js

--- movie-watchlist-backend/routes/movies.js
+++ movie-watchlist-backend/routes/movies.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const fs = require('fs-extra');
 const router = express.Router();
-const path = require('path');
-
-const filePath = path.join(__dirname, 'db', 'moviedb.json');
-fs.ensureDirSync(path.dirname(filePath));
+const filePath = './db/moviedb.json';
 
 const readMovies = async () => {
    try {
